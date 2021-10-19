e532885bbc886e779ef9f4c2ce7fe51a6a556618
juan <juan.grisalesg@upb.edu.co>
2021-10-18 23:44:01 -0500

    cambios menores

--- app/src/main/java/com/example/chatblenativo/Inicio.java
+++ app/src/main/java/com/example/chatblenativo/Inicio.java
@@ -6,21 +6,42 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Inicio extends AppCompatActivity {
-
     private Context context;
+    private Button btnSalir;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
-
         context = this;
+
+        btnSalir = findViewById(R.id.btn_salir);
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finish();
+
+                /*
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                 */
+            }
+        });
     }
 
 
-    public void MainAct(View view){
+    public void principal(View view){
         Intent siguiente = new Intent(context, MainActivity.class);
         startActivity(siguiente);
     }
+
+    public void salir(View view){
+
+    }
+
 }
